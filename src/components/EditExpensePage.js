75d94af952c5ff428d99c0ea2a3cf746747716c9
asuka0527 <junizzanakajima@gmail.com>
2020-12-29 21:12:06 +0900

    startRemoveExpenses intergrated

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseFrom from "./ExpenseForm";
-import { editExpense, removeExpense } from "../actions/expenses";
+import {
+  editExpense,
+  removeExpense,
+  startRemoveExpense,
+} from "../actions/expenses";
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
@@ -10,7 +14,7 @@ export class EditExpensePage extends React.Component {
   };
 
   onRemove = () => {
-    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.startRemoveExpense({ id: this.props.expense.id });
     this.props.history.push("/");
   };
 
@@ -32,7 +36,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (data) => dispatch(removeExpense(data)),
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
