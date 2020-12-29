185b0c8dc394893f1cc170ba392d5aa09480f6b8
asuka0527 <junizzanakajima@gmail.com>
2020-12-29 21:44:58 +0900

    Setup startEditExpense

--- src/test/actions/expense.test.js
+++ src/test/actions/expense.test.js
@@ -8,6 +8,7 @@ import {
   setExpenses,
   startSetExpenses,
   startRemoveExpense,
+  startEditExpense,
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
@@ -63,6 +64,29 @@ test("should setup edit expense action object", () => {
     type: "EDIT_EXPENSE",
     id: "123abc",
     updates: { note: "New note" },
+  });
+});
+
+// startEditExpense
+
+test("should edit expense from firebase", (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 500 };
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: "EDIT_EXPENSE",
+      id,
+      updates,
+    });
+    return database
+      .ref(`expenses/${id}`)
+      .once("value")
+      .then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+      });
   });
 });
 
